570e43954a79b3dca818b321e134e91f2c72946b
Albert Saucedo <saucedoa7@gmail.com>
2014-11-11 11:25:12 -0600

    Initial Commit

---
+++ Twitter_FB_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter_FB_ObjC
+//
+//  Created by Albert Saucedo on 11/11/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter_FB_ObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Twitter_FB_ObjC
+//
+//  Created by Albert Saucedo on 11/11/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
